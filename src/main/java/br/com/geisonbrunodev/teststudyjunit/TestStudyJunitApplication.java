2394ff2572185d67292d4fbb0a75ca2bbe24dec1
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-04 20:54:03 -0300

    first commit

---
+++ src/main/java/br/com/geisonbrunodev/teststudyjunit/TestStudyJunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.geisonbrunodev.teststudyjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStudyJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestStudyJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/geisonbrunodev/teststudyjunit/TestStudyJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geisonbrunodev.teststudyjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStudyJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
