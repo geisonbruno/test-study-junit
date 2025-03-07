546f4ecc5d593c014ce3cbc3a12a682387170b39
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-06 21:02:33 -0300

    feat: class UserServiceImpl findById

---
+++ src/main/java/br/com/geisonbrunodev/teststudyjunit/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.geisonbrunodev.teststudyjunit.services.impl;
+
+import br.com.geisonbrunodev.teststudyjunit.domain.User;
+import br.com.geisonbrunodev.teststudyjunit.repositories.UserRepository;
+import br.com.geisonbrunodev.teststudyjunit.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
