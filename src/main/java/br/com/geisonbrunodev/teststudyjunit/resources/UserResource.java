8f1b37a59596930aecfaef08b256841bacea1995
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-06 21:02:47 -0300

    feat: class UserResource findById

--- src/main/java/br/com/geisonbrunodev/teststudyjunit/resources/UserResource.java
+++ src/main/java/br/com/geisonbrunodev/teststudyjunit/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.geisonbrunodev.teststudyjunit.resources;
 
 import br.com.geisonbrunodev.teststudyjunit.domain.User;
+import br.com.geisonbrunodev.teststudyjunit.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(id, "Maria Brown", "m@gmail.com", "123456"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 }
