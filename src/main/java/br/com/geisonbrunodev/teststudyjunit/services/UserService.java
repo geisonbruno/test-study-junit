f5095591deec8f990c61781e798805c830917858
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-06 21:02:07 -0300

    feat: interface UserService findById

---
+++ src/main/java/br/com/geisonbrunodev/teststudyjunit/services/UserService.java
@@ -0,0 +1,11 @@
+package br.com.geisonbrunodev.teststudyjunit.services;
+
+import br.com.geisonbrunodev.teststudyjunit.domain.User;
+
+public interface UserService {
+
+    User findById(Integer id);
+
+
+
+}
