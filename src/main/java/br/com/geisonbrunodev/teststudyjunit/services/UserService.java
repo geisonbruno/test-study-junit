150a3d2ae80007530181d8cdd839d62dfc9be019
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-07 15:26:20 -0300

    feat: endpoint findAll p/ User

--- src/main/java/br/com/geisonbrunodev/teststudyjunit/services/UserService.java
+++ src/main/java/br/com/geisonbrunodev/teststudyjunit/services/UserService.java
@@ -2,10 +2,12 @@ package br.com.geisonbrunodev.teststudyjunit.services;
 
 import br.com.geisonbrunodev.teststudyjunit.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
 
-
+    List<User> findAll();
 
 }
--- src/main/java/br/com/geisonbrunodev/teststudyjunit/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/geisonbrunodev/teststudyjunit/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.geisonbrunodev.teststudyjunit.services.exceptions.ObjectNotFoundEx
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,4 +21,9 @@ public class UserServiceImpl implements UserService {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
 }
